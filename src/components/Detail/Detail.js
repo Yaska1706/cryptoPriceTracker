7797699a45bac4bff29cfd10a6020cd395443a61
yaska1706 <yasinkinyua@gmail.com>
2020-06-19 20:19:02 +0300

    Refactoring

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,12 +1,93 @@
 import React from 'react'
+import { API_URL } from '../../Config'
+import { ResponseHandler,changePercent } from '../../Helper'
+import Loading from '../common/Loading' 
 import './Detail.css'
 
 class Detail extends React.Component {
+    constructor(){
+        super();
+
+        this.state ={
+            currency: {},
+            loading: false,
+            error: null
+        };
+    }
+
+    componentDidMount(){
+
+        this.setState({
+            loading: true
+        })
+
+        const currencyId = this.props.match.params.id;
+
+        fetch(`${API_URL}/cryptocurrencies/${currencyId}`) 
+        .then(ResponseHandler)
+        .then((currency) =>{
+            this.setState({
+                loading:false,
+                error: null,
+                currency
+            })
+            
+        } )
+        .catch((error) => {
+            this.setState({
+                loading:false,
+                error: error.errorMessage
+            });
+        })  
+            
+    }
     render(){
+
+        const {loading,error,currency} = this.state
+
+        console.log('currency',currency);
+
+        if(loading){
+            return <div className='loading-container'><Loading/></div>
+        }
+
+        if(error){
+        return <div className='error'>{error}</div>
+        }
+
         return(
-            <div>
-                    Detail page
-            </div>
+            <div className="Detail">
+        <h1 className="Detail-heading">
+          {currency.name} ({currency.symbol})
+        </h1>
+
+        <div className="Detail-container">
+          <div className="Detail-item">
+            Price <span className="Detail-value">$ {currency.price}</span>
+          </div>
+          <div className="Detail-item">
+            Rank <span className="Detail-value">{currency.rank}</span>
+          </div>
+          <div className="Detail-item">
+            24H Change
+            <span className="Detail-value">{changePercent(currency.percentChange24h)}</span>
+          </div>
+          <div className="Detail-item">
+            <span className="Detail-title">Market cap</span>
+            <span className="Detail-dollar">$</span>
+            {currency.marketCap}
+          </div>
+          <div className="Detail-item">
+            <span className="Detail-title">24H Volume</span>
+            <span className="Detail-dollar">$</span>
+            {currency.volume24h}
+          </div>
+          <div className="Detail-item">
+            <span className="Detail-title">Total supply</span>
+            {currency.totalSupply}
+          </div>
+        </div>
+      </div>
         )
 
     }
